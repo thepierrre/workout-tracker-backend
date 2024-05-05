39dfd43beed258fc44356ddbc3b4b9136cb27187
thepierrre <piotr.owczarczyk98@gmail.com>
2024-05-05 10:35:21 +0200

    created endpoints for category

--- src/main/java/com/example/gymapp/repositories/WorkoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gymapp.repositories;
-
-import com.example.gymapp.domain.entities.WorkoutEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface WorkoutRepository extends JpaRepository<WorkoutEntity, UUID> {
-}
