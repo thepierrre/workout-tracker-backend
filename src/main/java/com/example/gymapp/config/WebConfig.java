1c7dace488f8e0c242ef3d2dcd4f3409cae9d900
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-19 14:24:33 +0200

    adjusted cors policies

--- src/main/java/com/example/gymapp/config/WebConfig.java
+++ src/main/java/com/example/gymapp/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(("http://localhost:5173"))
+                .allowedOrigins(("https://piotrs-workout-tracker.netlify.app/"))
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
