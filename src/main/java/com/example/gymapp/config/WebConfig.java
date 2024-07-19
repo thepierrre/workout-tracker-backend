a5aa8acd6f9fbb83f7d41de78ea0f97a12965f83
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-19 15:38:42 +0200

    test

--- src/main/java/com/example/gymapp/config/WebConfig.java
+++ src/main/java/com/example/gymapp/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(("https://piotrs-workout-tracker.netlify.app/"))
+                .allowedOrigins("https://piotrs-workout-tracker.netlify.app", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
