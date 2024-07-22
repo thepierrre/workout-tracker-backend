f88ed4c55a377167f6f2d9e9d05d357797ad6dc2
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-22 19:39:15 +0200

    test

--- src/main/java/com/example/gymapp/services/AuthService.java
+++ src/main/java/com/example/gymapp/services/AuthService.java
@@ -70,7 +70,7 @@ public class AuthService {
         cookie.setMaxAge(7 * 24 * 60 * 60);
 
         response.addCookie(cookie);
-        response.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=Strict; Secure");
+        response.setHeader("Set-Cookie", "key=value; HttpOnly; SameSite=None; Secure");
 
         return "User \"" + loginDto.getUsername() + "\" logged in.";
     }
