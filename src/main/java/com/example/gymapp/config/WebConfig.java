831984782b4f32dfcc2d23c8ffd0881563cf306f
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-23 08:21:31 +0200

    test

--- src/main/java/com/example/gymapp/config/WebConfig.java
+++ src/main/java/com/example/gymapp/config/WebConfig.java
@@ -1,46 +1,25 @@
 package com.example.gymapp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin
 public class WebConfig implements WebMvcConfigurer {
 
-        @Bean
-        public CorsFilter corsFilter() {
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            CorsConfiguration config = new CorsConfiguration();
-
-            // Add allowed origins
-            config.addAllowedOriginPattern("https://piotrs-workout-tracker.netlify.app");
-            config.addAllowedOriginPattern("https://workout-tracker-piotr-client-4d82d4dcd626.herokuapp.com/");
-            config.addAllowedOriginPattern("https://my-workout-tracker.work");
-            config.addAllowedOriginPattern("https://client.my-gym-tracker.work");
-
-            config.addAllowedMethod("*");
-            config.addAllowedHeader("*");
-            config.setAllowCredentials(true);
-            source.registerCorsConfiguration("/**", config);
-            return new CorsFilter(source);
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "https://piotrs-workout-tracker.netlify.app",
+                        "https://workout-tracker-piotr-client-4d82d4dcd626.herokuapp.com/",
+                        "https://my-workout-tracker.work",
+                        "https://client.my-gym-tracker.work"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns(
-//                        "https://piotrs-workout-tracker.netlify.app",
-//                        "https://workout-tracker-piotr-client-4d82d4dcd626.herokuapp.com",
-//                        "https://my-workout-tracker.work",
-//                        "https://client.my-gym-tracker.work"
-//                )
-//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+}
