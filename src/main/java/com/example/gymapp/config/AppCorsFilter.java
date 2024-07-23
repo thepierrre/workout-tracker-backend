83fce8c1ff0a5997da53664df8fdf18074b1a1ce
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-23 09:02:51 +0200

    test

--- src/main/java/com/example/gymapp/config/AppCorsFilter.java
+++ src/main/java/com/example/gymapp/config/AppCorsFilter.java
@@ -4,11 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 //@Configuration
 //public class WebConfig implements WebMvcConfigurer {
@@ -32,24 +36,18 @@ import java.io.IOException;
 @Component
 public class AppCorsFilter extends CorsFilter {
 
-    public static final String API_METHODS = "POST, GET, OPTIONS, DELETE, PUT, PATCH";
-
     public AppCorsFilter(CorsConfigurationSource configSource) {
         super(configSource);
     }
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-
-        //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Origin", "https://client.my-gym-tracker.work");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", API_METHODS);
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
-        filterChain.doFilter(request, response);
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://client.my-gym-tracker.work"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST, DELETE, PUT, PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
