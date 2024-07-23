5e9779d6871eb61b44537649d76bb96648ae8cdd
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-23 08:50:05 +0200

    test

---
+++ src/main/java/com/example/gymapp/config/AppCorsFilter.java
@@ -0,0 +1,54 @@
+package com.example.gymapp.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.io.IOException;
+
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns(
+//                        "https://piotrs-workout-tracker.netlify.app",
+//                        "https://workout-tracker-piotr-client-4d82d4dcd626.herokuapp.com/",
+//                        "https://my-workout-tracker.work",
+//                        "https://client.my-gym-tracker.work",
+//                        "http://localhost:5173"
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
+
+@Component
+public class AppCorsFilter extends CorsFilter {
+
+    public static final String API_METHODS = "POST, GET, OPTIONS, DELETE, PUT, PATCH";
+
+    public AppCorsFilter(CorsConfigurationSource configSource) {
+        super(configSource);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", API_METHODS);
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        filterChain.doFilter(request, response);
+    }
+
+}
